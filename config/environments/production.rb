a33afffedd50bc5df20b639c229f5d3221862e41
Jimmy Jones <jimjones26@gmail.com>
2014-04-12 22:10:59 -0400

    possible fix for heroku AW3 image upload error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,9 @@ config.i18n.fallbacks = true
   # sets paperclip to upload images to amazon s3
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => 'http',
+    :bucket => ENV['AWS_BUCKET'],
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
